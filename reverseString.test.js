bd14b0f1de4cea0e887f4ff08c6cf47fdef04022
David Kasilia <83514256+David-Kasilia@users.noreply.github.com>
2022-08-17 01:18:52 +0300

    Add test to reverse string

--- reverseString.test.js
+++ reverseString.test.js
@@ -3,3 +3,7 @@ const reverseString = require('./reverseString')
 test('Reverse String is', () => {
     expect(reverseString('length')).toBe('htgnel')
 })
+
+test('Reverse String is', () => {
+    expect(reverseString('least')).toBe('tsael')
+})
