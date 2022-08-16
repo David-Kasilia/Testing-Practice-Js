5393a2bbe8c6d6eabdede557a03aa4adec49c771
David Kasilia <83514256+David-Kasilia@users.noreply.github.com>
2022-08-17 01:14:26 +0300

    task one and two complete

---
+++ reverseString.test.js
@@ -0,0 +1,5 @@
+const reverseString = require('./reverseString')
+
+test('Reverse String is', () => {
+    expect(reverseString('length')).toBe('htgnel')
+})
--- stringLength.test.js
+++ stringLength.test.js
@@ -3,3 +3,11 @@ const stringLength = require('./stringLength')
 test('String length is', () => {
     expect(stringLength('length')).toBe(6)
 })
+
+test('String length is', () => {
+    expect(stringLength('b')).toBe(1)
+})
+
+test('String length is', () => {
+    expect(stringLength('anonymous')).toBe(9)
+})
